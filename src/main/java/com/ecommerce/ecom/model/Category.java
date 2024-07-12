520c7c5cc4d47c0778fde243f305665da919e939
Mzg. <jchallaksantos@gmail.com>
2024-07-12 16:46:00 -0300

    lbk+catjpa

--- src/main/java/com/ecommerce/ecom/model/Category.java
+++ src/main/java/com/ecommerce/ecom/model/Category.java
@@ -1,36 +1,26 @@
 package com.ecommerce.ecom.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "id")
     private Long categoryId;
+
+    @JoinColumn(name = "name")
     private String categoryName;
 
 
-    public Category(){}
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-
-
-
-
-    }
 }
+
--- src/main/java/com/ecommerce/ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/ecom/service/CategoryServiceImpl.java
@@ -1,55 +1,53 @@
 package com.ecommerce.ecom.service;
 
+import com.ecommerce.ecom.Repository.CategoryRepository;
 import com.ecommerce.ecom.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    CategoryRepository categoryRepository;
 
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
 
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
-        categories.remove(category);
+        Category category = categoryRepository.findById(categoryId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        categoryRepository.delete(category);
         return "Category with Id: " + categoryId + " deleted successfully";
     }
 
     @Override
-    public Category updateCategory(Category category, Long categoryId){
-            Optional<Category> optionalCategory = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).
-                    findFirst();
-
-            if (optionalCategory.isPresent()) {
-                Category existingCategory = optionalCategory.get();
-                existingCategory.setCategoryName(category.getCategoryName());
-                return existingCategory;
-            } else {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-            }
-        }
-
-
-
+    public Category updateCategory(Category category, Long categoryId) {
+        Category savedCategory = categoryRepository.findById(categoryId).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+        category.setCategoryId(categoryId); // Corrigido aqui
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+    }
 }
 
 
